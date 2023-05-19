1004807d249844961af8eb6356a9781e8811504b
RS <132225296+rsftb@users.noreply.github.com>
2023-05-19 22:07:26 +0200

    Update Decimals.py

--- Decimals.py
+++ Decimals.py
@@ -260,23 +260,3 @@ class Decimals:
 
     def subtractionDecimals(self, other):
         raise NotImplementedError
-
-
-a = Decimals("234,2325375547563457824895627834623786473264789236478236478623784623789468836572")
-b = Decimals("1,235343287467864235423546174789123891293")
-c = a + b
-print(c)
-
-Decimals.set_separator('.')
-c = a + b
-print(c)
-
-
-# For comparison
-import decimal
-decimal.getcontext().prec = 1000
-
-a = decimal.Decimal("234.2325375547563457824895627834623786473264789236478236478623784623789468836572")
-b = decimal.Decimal("1.235343287467864235423546174789123891293")
-c = a + b
-print(c)
